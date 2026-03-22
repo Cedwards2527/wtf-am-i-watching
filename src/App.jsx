2bca65423c0a0172d2e427ec88ce591ce9006542
Cedwards2527 <cedwards2527@gmail.com>
2026-03-21 22:48:50 -0400

    Create Header css

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
 import MovieOfTheDay from "./pages/MovieOfTheDay/MovieOfTheDay";
 import RandomMovie from "./pages/RandomMovie/RandomMovie";
+import About from "./pages/About/About";
 import "./App.css";
 
 function App() {
@@ -12,9 +13,10 @@ function App() {
       <div className="page__content">
         <Header />
         <Routes>
-          <Route path="/Home" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/movie-of-the-day" element={<MovieOfTheDay />} />
           <Route path="/random" element={<RandomMovie />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </div>
     </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,21 @@
 import wtfLogo from "../../assets/wtf-logo.jpg";
+import "./Header.css";
 
 function Header() {
   return (
     <header className="header">
       <img className="header__logo" src={wtfLogo} alt="wtf-logo" />
-      <button className="header__movie-of-the-day-btn" type="button">
-        Movie Of The Day
-      </button>
-      <button className="header__random-movie-btn" type="button">
-        Random Movie
-      </button>
+      <div className="header__controls">
+        <button className="header__movie-of-the-day-btn" type="button">
+          Movie Of The Day
+        </button>
+        <button className="header__random-movie-btn" type="button">
+          WTF Should I Watch Now?
+        </button>
+        <button className="header__about" type="button">
+          About WTF Am I Watching
+        </button>
+      </div>
     </header>
   );
 }
