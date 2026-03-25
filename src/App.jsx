dfe85fd5010a28fd6772cac7a6a9905739fe1b49
Cedwards2527 <cedwards2527@gmail.com>
2026-03-25 13:24:41 -0400

    Fetch and render first movie from API

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <div className="page__content">
         <Header />
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/movie-of-the-day" element={<MovieOfTheDay />} />
           <Route path="/random" element={<RandomMovie />} />
           <Route path="/about" element={<About />} />
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,27 @@
+import { useEffect, useState } from "react";
+import { getMovie } from "../../api/tmdb";
+import "./Home.css";
+
 function Home() {
-  return <h1>Home page</h1>;
+  const [movie, setMovie] = useState(null);
+  useEffect(() => {
+    getMovie(24804)
+      .then((data) => {
+        setMovie(data);
+      })
+
+      .catch(console.error);
+  }, []);
+  return (
+    <>
+      {movie && <h2 className="movie-title">{movie.title}</h2>}
+      <img
+        className="movie-image"
+        src="https://image.tmdb.org/t/p/w500/u3oWQDz0JggzzsVlsuHY7XVxp5Y.jpg"
+        alt="Movie poster"
+      ></img>
+    </>
+  );
 }
 
 export default Home;
