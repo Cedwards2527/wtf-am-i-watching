1e82d41dd956f73f82737caf660c373e6ac6736e
Cedwards2527 <cedwards2527@gmail.com>
2026-03-25 13:44:01 -0400

    Create utils folder and image.js file

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { getMovie } from "../../api/tmdb";
+import { getPosterUrl } from "../../utils/image";
 import "./Home.css";
 
 function Home() {
@@ -15,11 +16,13 @@ function Home() {
   return (
     <>
       {movie && <h2 className="movie-title">{movie.title}</h2>}
-      <img
-        className="movie-image"
-        src="https://image.tmdb.org/t/p/w500/u3oWQDz0JggzzsVlsuHY7XVxp5Y.jpg"
-        alt="Movie poster"
-      ></img>
+      {movie && (
+        <img
+          className="movie-image"
+          src={getPosterUrl(movie.poster_path)}
+          alt="Movie poster"
+        ></img>
+      )}
     </>
   );
 }
